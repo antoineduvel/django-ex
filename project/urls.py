17f713a5daf8ea7de7cbcb97646dfb89fcd74ea4
Antoine DUVEL <antoineduvel@gmail.com>
2018-03-05 16:14:54 +0100

    using path

--- project/urls.py
+++ project/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import include, url, path
 from django.contrib import admin
 
 from welcome.views import index, health, home, view_article
@@ -13,7 +13,7 @@ urlpatterns = [
     url(r'^health$', health),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accueil$', home),
-    url(r'^article/<id_article>$', view_article),
+    path('article/<id_article>', view_article),
 ]
 
 if settings.DEBUG:
